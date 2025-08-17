dae1a01819bd9ee38c407587919160cac92e8bb7
Rohit Gomes <gomesrohit92@gmail.com>
2025-08-17 21:02:27 +0530

    Updated encoder and scaler codes

--- data_process/utils/encode_categorical.py
+++ data_process/utils/encode_categorical.py
@@ -1,12 +1,21 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-def encode_categorical(df, encoding_type="onehot"):
+def encode_categorical(df, encoding_type="onehot")-> tuple[pd.DataFrame, dict]:
+    """
+    Encodes categorical columns into numeric form.
+
+    Args:
+        df (pd.DataFrame): Input dataset.
+        encoding_type (str): "onehot" or "label".
+    Returns:
+        tuple: (Encoded DataFrame, mapping dictionary for label encoding)
+    """
     df_copy = df.copy()
     cat_cols = df_copy.select_dtypes(include=["object", "category"]).columns
 
     if encoding_type == "onehot":
-        return pd.get_dummies(df_copy, columns=cat_cols), {}
+        return (pd.get_dummies(df_copy, columns=cat_cols,drop_first=True), {})
 
     elif encoding_type == "label":
         mapping = {}
@@ -14,7 +23,7 @@ def encode_categorical(df, encoding_type="onehot"):
             le = LabelEncoder()
             df_copy[col] = le.fit_transform(df_copy[col].astype(str))
             mapping[col] = dict(zip(le.classes_, le.transform(le.classes_)))
-        return df_copy, mapping
+        return (df_copy, mapping)
 
     else:
         raise ValueError("encoding_type must be 'onehot' or 'label'")
--- data_process/utils/scale_numeric.py
+++ data_process/utils/scale_numeric.py
@@ -2,7 +2,16 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
 import numpy as np
 
-def scale_numeric(df, scaler_type="standard"):
+def scale_numeric(df, scaler_type="standard")-> tuple[pd.DataFrame, object]:
+    """
+    Scales numeric columns.
+
+    Args:
+        df (pd.DataFrame): Input dataset.
+        scaler_type (str): "standard" or "minmax".
+    Returns:
+        tuple: (Scaled DataFrame, fitted scaler object)
+    """
     if scaler_type == "standard":
         scaler = StandardScaler() 
     else :
@@ -10,4 +19,8 @@ def scale_numeric(df, scaler_type="standard"):
     num_cols = df.select_dtypes(include=[np.number]).columns
     df_scaled = df.copy()
     df_scaled[num_cols] = scaler.fit_transform(df[num_cols])
-    return df_scaled, scaler
+    return (df_scaled, scaler)
+
+data = pd.read_csv(r"C:\Users\RJ\Downloads\archive (3)\tested.csv") 
+print(data)
+print(scale_numeric(data))
