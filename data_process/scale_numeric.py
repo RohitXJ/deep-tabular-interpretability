Scaling issues fixed + eval and null checker updated

data_process/scale_numeric.py
@@ -2,36 +2,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
 import numpy as np
 
-def scale_numeric(df:pd.DataFrame,target_col:str, scaler_type="standard")-> tuple[pd.DataFrame, object]:
-    """
-    Scales numeric columns.
-
-    Args:
-        df (pd.DataFrame): Input dataset.
-        scaler_type (str): "standard" or "minmax".
-    Returns:
-        tuple: (Scaled DataFrame, fitted scaler object)
-    """
-    target_df = df[target_col]
-    if scaler_type == "standard":
-        scaler = StandardScaler() 
-    else :
-        scaler = MinMaxScaler()
-    num_cols = df.select_dtypes(include=[np.number]).columns
-    df_scaled = df.copy()
-    df_scaled[num_cols] = scaler.fit_transform(df[num_cols])
-    df_scaled[target_col]=target_df
-    return (df_scaled, scaler)
-
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-def scale_numeric(df: pd.DataFrame, 
-               target_col: str, 
-               domain_name: str, 
-               model_name: str, 
-               prediction_type: str,
-               scaler_type="standard"):
+def scale_numeric(df: pd.DataFrame, target_col: str, domain_name: str, model_name: str, prediction_type: str,scaler_type="standard")-> tuple[pd.DataFrame, object]:
     """
     Scale data with rules:
     - Numerical feature columns (excluding target) are always scaled.
@@ -46,7 +17,7 @@ def scale_numeric(df: pd.DataFrame,
     df_scaled = df.copy()
 
     # Always scale numerical feature columns (excluding target)
-    num_cols = df.select_dtypes(include=[np.number]).columns.tolist()
+    num_cols = df_scaled.select_dtypes(include=[np.number]).columns.tolist()
     if target_col in num_cols:
         num_cols.remove(target_col)
     if num_cols:  # scale only if there are numeric columns
@@ -59,4 +30,4 @@ def scale_numeric(df: pd.DataFrame,
         except KeyError:
             raise ValueError("Value missing Error: Target column not found in dataframe")
 
-    return df_scaled
+    return (df_scaled, scaler)